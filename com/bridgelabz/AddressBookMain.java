147aeb8e9745f595abe8692470fcb5dfa49616ab
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-10-17 23:17:16 +0530

    Added AddressBookSystem UC4

--- com/bridgelabz/AddressBookMain.java
+++ com/bridgelabz/AddressBookMain.java
@@ -2,19 +2,16 @@ package com.bridgelabz;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class AddressBookMain {
     public static void main(String[] args) {
-
-      ArrayList<AddressBook> bookArrayList = new ArrayList<>();
-      Scanner scan = new Scanner(System.in);
+        ArrayList<AddressBook> bookArrayList = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
       int choice;
         do {
-            System.out.println("1.Add");
-            System.out.println("2.Display");
-            System.out.println("0 to EXIT Program !!!.");
-            System.out.println("-----------");
+            System.out.println("1.Add \t2.Display \t 3.Update \t 4.Delete \nPress 0 to EXIT Program !!!.");
             System.out.println("Enter your choice : ");
             choice = scan.nextInt();
             switch (choice) {
@@ -22,15 +19,42 @@ public class AddressBookMain {
                   AddressBook addressBook = new AddressBook();
                   addressBook.contactDetails();
                   bookArrayList.add(addressBook);
-                    break;
+                  break;
                 case 2:
-                    System.out.println(bookArrayList);
+                    Iterator<AddressBook> iterator1 = bookArrayList.iterator();
+                    while (iterator1.hasNext()) {
+                       addressBook = iterator1.next();
+                        System.out.println(addressBook);
+                    }
                     break;
+                case 3:
+                    System.out.println("Enter first name : ");
+                    ListIterator<AddressBook> iterator = bookArrayList.listIterator();
+                    String name = scan.next();
+                    while (iterator.hasNext()){
+                        addressBook = iterator.next();
+                        if (name.equals(addressBook.contact.getFirstName())){
+                            addressBook.contactDetails();
+                            iterator.set(addressBook);
+                        }else {
+                            System.out.println("Enter data not found.");
+                        }
+                    }
+                    break;
+                case 4:
+                    System.out.println("Enter first name : ");
+                    name = scan.next();
+                    Iterator<AddressBook> iterator2 = bookArrayList.iterator();
+                    while (iterator2.hasNext()){
+                        addressBook = iterator2.next();
+                        if (name.equals(addressBook.contact.getFirstName())){
+                            iterator2.remove();
+                        }
+                    }
                 default:
                     if (choice!=0)
                     System.out.println("Enter valid inputs.");
             }
-
         }while (choice!=0);
 
     }
